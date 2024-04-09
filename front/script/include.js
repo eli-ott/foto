3f055d66282324ea65f7086d7178c17de1c0b565
eli-ott <eliottbidault@gmail.com>
2024-04-09 14:35:18 +0200

    Rename variables and added default html

--- front/script/include.js
+++ front/script/include.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-	let e = document.getElementsByTagName('include');
+	let elements = document.getElementsByTagName('include');
 
 	// go through all the include tags and fetch the html to add it
-	for (var t = 0; t < e.length; t++) {
-		let a = e[t];
-		n(e[t].attributes.src.value, function (e) {
-			a.insertAdjacentHTML('afterend', e), a.remove();
+	for (var i = 0; i < elements.length; i++) {
+		let element = elements[i];
+		n(elements[i].attributes.src.value, function (html) {
+			element.insertAdjacentHTML('afterend', html), element.remove();
 		});
 	}
 
 	/**
 	 * Fetch and return the html
 	 *
-	 * @param {string} e The source to fetch
-	 * @param {Function} t The callback to add the fetched HTML to the DOM
+	 * @param {string} src The source to fetch
+	 * @param {Function} callback The callback to add the fetched HTML to the DOM
 	 */
-	function n(e, t) {
-		fetch(e)
-			.then(e => e.text())
-			.then(e => t(e));
+	function n(src, callback) {
+		fetch(src)
+			.then(res => res.text())
+			.then(ret => callback(ret));
 	}
 });
