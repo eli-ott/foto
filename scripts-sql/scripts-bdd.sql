89be5ecd82a903c128099ab27295d5ef7a6ebe3a
baptiste vauthier <ba.vauthier@gmail.com>
2024-04-09 14:16:08 +0200

    fix erreur sql

--- scripts-sql/scripts-bdd.sql
+++ scripts-sql/scripts-bdd.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE Foto;
 USE Foto;
 
-CREATE TABLE user (
+CREATE TABLE `user` (
     id_user INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     pseudo varchar(30) NOT NULL,
     nom varchar(50) NOT NULL,
@@ -10,10 +10,10 @@ CREATE TABLE user (
     age INT NOT NULL,
     type_photo_pref varchar(50) NOT NULL,
     date_creation DATETIME NOT NULL,
-    warn BOOLEAN NOT NULL,
+    warn BOOLEAN NOT NULL
 );
 
-CREATE TABLE photo (
+CREATE TABLE `photo` (
     id_photo INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     id_user INT NOT NULL,
     titre varchar(50) NOT NULL,
@@ -24,10 +24,10 @@ CREATE TABLE photo (
     CONSTRAINT id_user_FK FOREIGN KEY (id_user) REFERENCES user(id_user)
 );
 
-CREATE TABLE envoyer (
+CREATE TABLE `envoyer` (
     id_envoyeur INT NOT NULL,
     id_receveur INT NOT NULL,
-    [message] text NOT NULL,
+    `message` text NOT NULL,
     CONSTRAINT id_envoyeur_FK FOREIGN KEY (id_envoyeur) REFERENCES user(id_user),
-    CONSTRAINT id_receveur_FK FOREIGN KEY (id_receveur) REFERENCES user(id_user),
+    CONSTRAINT id_receveur_FK FOREIGN KEY (id_receveur) REFERENCES user(id_user)
 );
